4dbbb05a917f14355a1343f79f425a0b87aafcb0
chandan <chandanchethan321@gmail.com>
2024-09-28 19:12:02 +0530

    added analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import { Analytics } from "@vercel/analytics/react"
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -112,6 +113,7 @@ export default function RootLayout({
         </script> */}
       </head>
       <body className={`${inter.className} antialiased`}>{children}</body>
+      <Analytics />
     </html>
   );
 }
